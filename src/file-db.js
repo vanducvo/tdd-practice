d659383e68b833a656ff8bb4ccc86a5f28144e9a
Van Duc Vo <vanducvo.dev@gmail.com>
2020-12-23 09:52:34 +0700

    green

--- src/file-db.js
+++ src/file-db.js
@@ -1,6 +1,13 @@
+const path = require('path');
+
 class FileDB {
-  constructor(filepath){
+  constructor(filepath, name){
     this._filepath = filepath;
+    this._name = name;
+  }
+
+  getPathFile(){
+    return path.join(this._filepath, this._name);
   }
 }
 
--- src/file-db.test.js
+++ src/file-db.test.js
@@ -1,15 +1,28 @@
 const fs = require('fs');
 const FileDB = require('./file-db');
+const path = require('path');
 
 describe('File DB', () => {
 
   test('can constuctor with file path', () => {;
     const dbPath = getDBPathFromEnviroment();
-    
-    const fileDB = new FileDB(dbPath);
+    const name = 'mydb';
+
+    const fileDB = new FileDB(dbPath, name);
     
     expect(fileDB).toBeInstanceOf(FileDB);
     expectObjectContainValue(fileDB, dbPath);
+    expectObjectContainValue(fileDB, name);
+  });
+
+  test('can get path file', () => {
+    const dbPath = getDBPathFromEnviroment();
+    const name = 'mydb';
+
+    const fileDB = new FileDB(dbPath, name);
+
+
+    expect(fileDB.getPathFile()).toEqual(path.join(dbPath, name));
   });
 });
 
